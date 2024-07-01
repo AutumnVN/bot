079a193f57971526ccf0fdff5a125639df67caeb
AutumnVN <autumnvnchino@gmail.com>
2024-07-02 06:15:08 +0700

    error log channel

--- src/Client.ts
+++ src/Client.ts
@@ -1,8 +1,10 @@
 import { Client } from 'oceanic.js';
 
+import { codeBlock, send } from './utils';
+
 process.loadEnvFile();
-process.on('unhandledRejection', console.error);
-process.on('uncaughtException', console.error);
+process.on('unhandledRejection', logError);
+process.on('uncaughtException', logError);
 
 export const client = new Client({
     auth: `Bot ${process.env.TOKEN}`,
@@ -17,4 +19,13 @@ export let ownerID: string;
 
 export function setOwnerID(id: string) {
     ownerID = id;
+}
+
+function logError(error: unknown) {
+    console.error(error);
+
+    const { ERROR_LOG_CHANNEL_ID } = process.env;
+    if (!ERROR_LOG_CHANNEL_ID || !(error instanceof Error)) return;
+
+    send(ERROR_LOG_CHANNEL_ID, codeBlock(error.stack || error.message));
 }
--- src/modules/error.ts
+++
@@ -1,3 +0,0 @@
-import { client } from '../Client';
-
-client.on('error', console.error);
